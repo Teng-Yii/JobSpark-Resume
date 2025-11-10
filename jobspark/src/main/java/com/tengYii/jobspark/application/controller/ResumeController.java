1cd8248cc76c04fbced323d9d86ab1138ef0fee3
Teng-Yii <wt10051520@163.com>
2025-11-11 00:12:28 +0800

    feat: 上传简历接口参数校验

--- jobspark/src/main/java/com/tengYii/jobspark/application/controller/ResumeController.java
+++ jobspark/src/main/java/com/tengYii/jobspark/application/controller/ResumeController.java
@@ -3,9 +3,11 @@ package com.tengYii.jobspark.application.controller;
 import com.tengYii.jobspark.application.dto.ResumeUploadRequest;
 import com.tengYii.jobspark.application.dto.ResumeUploadResponse;
 import com.tengYii.jobspark.application.service.ResumeApplicationService;
+import com.tengYii.jobspark.application.validate.ResumeValidator;
 import com.tengYii.jobspark.common.exception.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +29,7 @@ public class ResumeController {
         // 校验请求参数合法性
         String validationResult = ResumeValidator.validate(request);
         if(StringUtils.isNotBlank(validationResult)){
-            throw new ValidationException(validationResult);
+            throw new ValidationException(String.valueOf(HttpStatus.BAD_REQUEST.value()), validationResult);
         }
 
         ResumeUploadResponse response = resumeApplicationService.uploadResume(request);
